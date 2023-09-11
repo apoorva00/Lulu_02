308f2cddf2167f0f10b76dee52fe27ae4dc63d25
apoorva00 <apoorva.ashok2001@gmail.com>
2023-09-11 15:50:55 +0530

    Apoorva first commit

---
+++ src/Lulu_02/Lulu_02.java
@@ -0,0 +1,10 @@
+package Lulu_02;
+
+public class Lulu_02 {
+	public static void main(String args[]) {
+		System.out.println("TRYING TO EXPLORE TO GIT");
+		System.out.println("We will also explore local repo and remote repo");
+		System.out.println("We will also explore gitbash and github");
+	}
+		
+}
